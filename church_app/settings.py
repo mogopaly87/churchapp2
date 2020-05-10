61207c8e5fb92f4f1a31c473b5de11ce9f609a97
Chinonso Mogo <mogononso@gmail.com>
2020-05-10 18:31:17 -0230

    update

--- church_app/settings.py
+++ church_app/settings.py
@@ -154,6 +154,7 @@ AWS_STORAGE_BUCKET_NAME = 'church-app-mogononso' #os.environ.get('AWS_STORAGE_BU
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
