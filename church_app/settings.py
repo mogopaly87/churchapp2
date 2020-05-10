68798b7ae59fb385c495ea9b8ec335d9cf1dbf98
Chinonso Mogo <mogononso@gmail.com>
2020-05-10 12:02:15 -0230

    Initial commit

--- church_app/settings.py
+++ church_app/settings.py
@@ -161,6 +161,6 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 
-import dj_database_url 
-prod_db  =  dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# import dj_database_url 
+# prod_db  =  dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
