168c1a24085b8f2eac67572ce6f016a40e1ccac3
Chinonso Mogo <mogononso@gmail.com>
2020-05-10 18:27:35 -0230

    update

--- church_app/settings.py
+++ church_app/settings.py
@@ -156,7 +156,6 @@ AWS_DEFAULT_ACL = 'public-read'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
 
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
