4be6ccc840496252154fc08cfed1ad4d0287125d
mogopaly87 <mogononso@gmail.com>
2020-08-29 07:25:39 -0230

    added signature to profile & tax slip

--- church_app/settings.py
+++ church_app/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # DEVELOPMENT STATIC ROOT:
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
