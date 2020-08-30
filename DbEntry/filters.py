9cd57f1e0698d612b444dc3c197a2351e8665246
mogopaly87 <mogononso@gmail.com>
2020-08-30 09:13:16 -0230

    changed label name for filters

--- DbEntry/filters.py
+++ DbEntry/filters.py
@@ -3,6 +3,16 @@ import django_filters
 from django import forms
 
 class GivingFilter(django_filters.FilterSet):
+
+    # To change the default label names, override the __init__ method
+    # ... of 'FilterSet' using the __init__ method below
+    # Source: https://github.com/carltongibson/django-filter/issues/107
+
+    def __init__(self, *args, **kwargs):
+        super(GivingFilter, self).__init__(*args, **kwargs)
+        self.filters['giving_date__gt'].label = 'Start Date'
+        self.filters['giving_date__lt'].label = 'End Date'
+
     class Meta:
         model = GivingModel
         fields = {
